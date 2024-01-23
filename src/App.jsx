23346d31f83142c4a91aeae99a131094a9761fb1
santosh chulakimath <santoshchulakimath222@gmail.com>
2024-01-23 14:20:52 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
   }
 
   const updateTodo = (id, todo) => {
+    console.log(todo)
     setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
   }
 
@@ -19,7 +20,6 @@ function App() {
   }
 
   const toggleComplete = (id) => {
-    //console.log(id);
     setTodos((prev) =>
       prev.map((prevTodo) =>
         prevTodo.id === id ? {
@@ -49,15 +49,12 @@ function App() {
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
           <div className="mb-4">
-            {/* Todo form goes here */}
             <TodoForm />
           </div>
           <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
             {todos.map((todo) => (
-              <div key={todo.id}
-                className='w-full'
-              >
+              <div key={todo.id} className='w-full'>
                 <TodoItem todo={todo} />
               </div>
             ))}
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react'
-import { useTodo } from '../contexts/TodoContext';
+import React, { useEffect, useRef, useState } from 'react'
+import { useTodo } from '../contexts/';
 
 function TodoForm() {
     const [todo, setTodo] = useState('');
     const { addTodo } = useTodo();
+    const myref=useRef();
 
     const add= (e) => {
         e.preventDefault()
@@ -11,17 +12,19 @@ function TodoForm() {
         addTodo({todo, completed: false });
         setTodo('');
     }
-    // const onchangeHandler = (e) => {
-    //     setTodo(e.target.value)
-    // }
+    useEffect(()=>{
+        myref.current.focus();
+    },[])
+
     return (
         <form onSubmit={add} className="flex">
             <input
                 type="text"
                 placeholder="Write Todo..."
-                className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+                className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5 focus:bg-slate-900"
                 value={todo}
                 onChange={(e)=> setTodo(e.target.value)}
+                ref={myref}
             />
             <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0"
             >
